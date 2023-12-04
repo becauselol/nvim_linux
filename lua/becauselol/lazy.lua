d91fb986e1359bf3e5bb467929d0d0d8c9b0e5f3
becauselol <becauselol@github.com>
2023-12-04 15:35:47 +0800

    added mason but some linters cant be installed

--- lua/becauselol/lazy.lua
+++ lua/becauselol/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "becauselol.plugins" } }, {
+require("lazy").setup({ { import = "becauselol.plugins" }, {import = "becauselol.plugins.lsp"} }, {
 --  install = {
 --    colorscheme = { "nightfly" },
 --  },
