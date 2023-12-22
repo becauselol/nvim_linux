810188a12126bad75c31060e251129e47a91ebc4
becauselol <becauselol@github.com>
2023-12-22 12:01:18 +0800

    fix: call config setup

--- lua/becauselol/plugins/colorscheme.lua
+++ lua/becauselol/plugins/colorscheme.lua
@@ -1,18 +1,22 @@
 return {
-    "craftzdog/solarized-osaka.nvim",--"EdenEast/nightfox.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-        --theme = "dragon"
-    },
-    config = function()
-      -- load the colorscheme here
-      vim.o.background = ""
-      vim.cmd([[colorscheme carbonfox]])
-      require("nightfox").setup({
-          transparent = true
-      })
-      vim.cmd[[hi NormalNC guibg=None ctermbg=None]]
-      vim.cmd[[hi Normal guibg=NONE ctermbg=NONE]]
-    end
+  "craftzdog/solarized-osaka.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.cmd[[colorscheme solarized-osaka]]
+  end
 }
+--return {
+--    "craftzdog/solarized-osaka.nvim",--"EdenEast/nightfox.nvim",
+--    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--    priority = 1000, -- make sure to load this before all the other start plugins
+--    opts = {},
+--    config = function()
+--      -- load the colorscheme here
+--      --vim.cmd([[colorscheme carbonfox]])
+--      require("solarized-osaka").setup({
+--          --transparent = true
+--      })
+--    end
+--}
