1b770e65cd71d0c745cf7d225cced7efae64cd19
becauselol <becauselol@github.com>
2023-12-28 03:15:27 +0800

    feat: configure lsp and realise issue is missing node on linux

--- lua/becauselol/plugins/telescope.lua
+++ lua/becauselol/plugins/telescope.lua
@@ -3,6 +3,21 @@ return {
 -- or                              , branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' },
       config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+
+      telescope.setup({
+        defaults = {
+          path_display = { "truncate " },
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+              ["<C-j>"] = actions.move_selection_next, -- move to next result
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+          },
+        },
+      })
 
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Fuzzy find files in cwd" })
