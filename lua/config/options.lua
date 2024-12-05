b08e8e9ae425a338c10f5f99d3beb611d086f8cc
becauselol <gabesytf@gmail.com>
2024-12-05 14:08:38 +0800

    feat: add line number colours

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,12 @@ opt.wrap = true
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
+
+function LineNumberColors()
+	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#89dceb", bold = false })
+	vim.api.nvim_set_hl(0, "LineNr", { fg = "#cba6f7", bold = true })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#f38ba8", bold = false })
+end
+LineNumberColors()
+
+opt.spelllang = "en_gb"
